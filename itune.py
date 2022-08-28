5f4d166838f1735cc83c6231c420e8f37d03aa92
Kenny Chow <68889558+KennyChowPD@users.noreply.github.com>
2022-08-28 18:41:37 +0900

    commit

--- itune.py
+++ itune.py
@@ -7,6 +7,7 @@ if len(sys.argv) != 2:
 
 response = requests.get(
     "https://itunes.apple.com/search?entity=song&attribute=allArtistTerm&limit=50&term=" + sys.argv[1])
+print(response)
 
 print(json.dumps(response.json(), indent=2))
 
