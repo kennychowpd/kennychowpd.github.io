2e081f8d9ffffe25196720b8aec6b2c574f62e5c
Kenny Chow <68889558+KennyChowPD@users.noreply.github.com>
2022-08-31 15:08:34 +0900

    Matthew

--- indoor.py
+++
@@ -1,8 +0,0 @@
-def main():
-    
-    user_input = str(input("Say something: "))
-    user_input = user_input.lower()
-    print(user_input)
-    
-    
-main()
--- people.py
+++ people.py
@@ -1,23 +1,18 @@
 import csv
 
 people = []
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+home = (input("Where do your live? "))
 
 with open("people.csv", "a") as file:
-    name = input("What is your name? ")
-    age = int(input("What is your age? "))
-    home = (input("Where do your live? "))
-
-    file.write(f"{name},{age},{home}\n")
+    writer = csv.DictWriter(file, fieldnames=["name", "age", "home"])
+    writer.writerow({"name": name, "age": age, "home": home})
     
 with open("people.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        person = {"name": row["name"], "age": row["age"], "home": row["home"]}
-        people.append(person)
-        
-def get_name(person):
-    return person["name"]
-
+        people.append(row)
 
 for person in sorted(people, key=lambda person: person["name"]):
     print(f"{person['name']} is {person['age']} years old and lives in {person['home']}.")
